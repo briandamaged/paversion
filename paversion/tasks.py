949813afad853db48d0ba406176460c4e1311627
Brian Lauber <constructible.truth@gmail.com>
2013-02-17 17:21:23 -0500

    Updated setup.py to list Paver as an install requirement.

---
+++ paversion/tasks.py
@@ -0,0 +1,16 @@
+
+from paver.tasks import task
+
+@task
+def bump_patch():
+  pass
+
+
+@task
+def bump_minor():
+  pass
+
+
+@task
+def bump_major():
+  pass
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author = 'Brian Lauber',
     author_email = 'constructible.truth@gmail.com',
     packages = find_packages(exclude = ["tests.*", "tests"]),
+    install_requires = ["paver>=1.1.0"],
     test_suite = 'tests',
 )
 
